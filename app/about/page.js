a119079a37e8554caef80f53d9f8916958cbb9a0
Sarah <144402200+chimmies@users.noreply.github.com>
2024-02-01 08:34:22 -0800

    make pages

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+import 'next/link'
+
+const AboutPage = () => {
+    return (
+    <div>
+     <h1 className="text-7xl">AboutPage</h1> 
+  <Link href='/' className="text-2x">home page</Link>
+  </div>
+  );
+};
+  export default AboutPage;
